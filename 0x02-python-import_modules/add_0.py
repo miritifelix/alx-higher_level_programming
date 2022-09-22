fbe6bad0c0190b82173636f0273c087028173727
miritifelix <miriti9595@gmail.com>
2022-09-22 17:41:12 +0300

    imports

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def add(a, b):
-    """returns the value of a + b
-    Args:
-        a: first integer
-        b: second integer
-        
-    Returns:
-        The return value. a+b
-    """
+    """returns the value of a + b"""
     return (a + b)
